ad14f2e03e1bbb879944a85c053ad00ee6ddd70f
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-23 20:28:46 -0300

    Terceira Aula
    16min

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -4,10 +4,16 @@ import { Button } from '../components/Button';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
+import { FormEvent } from 'react';
+import { useState } from 'react';
 
 export function NewRoom() {
 	// const {user} = useAuth()
+	const [ newRoom, SetNewRoom ] = useState();
 
+	async function hadleCreateRoom(event: FormEvent) {
+		event.preventDefault();
+	}
 	return (
 		<div id="page-auth">
 			<aside>
@@ -19,7 +25,7 @@ export function NewRoom() {
 				<div className="main-content">
 					<img src={logoImg} alt="logo letmeask" />
 					<h2>Crie uma nova Sala</h2>
-					<form>
+					<form onSubmit={hadleCreateRoom}>
 						<input type="text" placeholder="Nome da Sala" />
 						<Button type="submit">Criar sala</Button>
 					</form>
