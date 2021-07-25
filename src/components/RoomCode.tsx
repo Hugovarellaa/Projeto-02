1c5590ddecd6a3149594fa4eacb9048dd447fde0
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-25 11:30:43 -0300

    Terceira Aula
    1hour

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,13 +1,21 @@
 import copyImg from '../assets/images/copy.svg';
 import '../styles/roomCode.scss';
 
-export function RoomCode() {
+interface RoomCodeProp {
+	code: string;
+}
+
+export function RoomCode(props: RoomCodeProp) {
+	function copyRoomCodeClipboard() {
+		navigator.clipboard.writeText(props.code);
+	}
+
 	return (
-		<button className="room-code">
+		<button className="room-code" onClick={copyRoomCodeClipboard}>
 			<div>
 				<img src={copyImg} alt="Copy room code" />
 			</div>
-			<span> Sala #141231231</span>
+			<span> Sala #{props.code}</span>
 		</button>
 	);
 }
