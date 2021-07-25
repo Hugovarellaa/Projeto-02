5d63b81783d158ca79d40447eef66c54c53a5cb9
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-25 11:22:52 -0300

    Terceira Aula
    57min

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,13 +1,13 @@
 import copyImg from '../assets/images/copy.svg';
 import '../styles/roomCode.scss';
 
-export function RoomCode(){
-  return (
-    <button className="room-code">
-      <div>
-        <img src={copyImg} alt="Copy room code" />
-      </div>
-      <span> Sala hauhuahuh</span>
-    </button>
-  )
+export function RoomCode() {
+	return (
+		<button className="room-code">
+			<div>
+				<img src={copyImg} alt="Copy room code" />
+			</div>
+			<span> Sala #141231231</span>
+		</button>
+	);
 }
