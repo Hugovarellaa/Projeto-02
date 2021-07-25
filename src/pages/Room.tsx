9112207867aea53327bc20923d1328fa4556841a
Hugo Alves Varella <71407198+Hugovarellaa@users.noreply.github.com>
2021-07-25 11:52:08 -0300

    Terceira Aula
    01:06:00

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,22 +3,46 @@ import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
 import '../styles/room.scss';
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 interface RoomParams {
 	id: string;
 }
 
-
 export function Room() {
+	const { user } = useAuth();
 	const params = useParams<RoomParams>();
+	const [ newQuestion, setNewQuestion ] = useState('');
+	const roomId = params.id;
 
+	async function handleSendQuestion() {
+		if (newQuestion.trim() === '') {
+			return;
+		}
+		if (!user) {
+			throw new Error('You must be logged in');
+		}
+		const question = {
+			content: newQuestion,
+			author: {
+				name: user.name,
+				avatar: user.avatar,
+			},
+			isHighLighted: false,
+			isAnswered: false
+		};
+
+		await database.ref(`rooms/${roomId}/questions`)
+	}
 
 	return (
 		<div id="page-room">
 			<header>
 				<div className="content">
 					<img src={logoImg} alt="" />
-					<RoomCode code={params.id} />
+					<RoomCode code={roomId} />
 				</div>
 			</header>
 			<main>
@@ -28,7 +52,11 @@ export function Room() {
 				</div>
 
 				<form>
-					<textarea placeholder="O que Voce quer Perguntar?" />
+					<textarea
+						placeholder="O que Voce quer Perguntar?"
+						onChange={(event) => setNewQuestion(event.target.value)}
+						value={newQuestion}
+					/>
 					<div className="form-footer">
 						<span>
 							Para enviar uma pergunta, <button> faca seu login</button>.
